b2b293171b2a2b83d488f52e65bd8074cf2e81b0
Charlie Kenney <me@ch4z.io>
2023-07-01 16:51:31 -0500

    enable recording mode

--- lua/keymap/ui.lua
+++ lua/keymap/ui.lua
@@ -4,9 +4,6 @@ local cmd = map.cmd
 local mappings = {
     -- TODO: search on current line <leader>\\, get line number, run :/%LINE
 
-    -- Disable stupid fucking recording mode
-    ["n|q"] = cmd("<Nop>"),
-
     -- Window navigation
     ["n|<leader>wH"] = cmd("<c-w>H"):noremap():desc("window: move left"),
     ["n|<leader>wJ"] = cmd("<c-w>J"):noremap():desc("window: move up"),
@@ -16,7 +13,6 @@ local mappings = {
     ["n|<leader>wj"] = cmd("<c-w>j"):noremap():desc("window: go up"),
     ["n|<leader>wk"] = cmd("<c-w>k"):noremap():desc("window: go down"),
     ["n|<leader>wl"] = cmd("<c-w>l"):noremap():desc("window: go right"),
-
     -- Buffer lifecycle/navigation
     ["n|<leader>q"] = cmd("q"):noremap():desc("buffer: quit"),
     ["n|<leader>wq"] = cmd("wq"):noremap():desc("buffer: save & quit"),
@@ -25,7 +21,6 @@ local mappings = {
     ["n|<leader>ka"] = cmd("xa!"):noremap():desc("buffer: kill all"),
     ["n|8"] = cmd("bprevious"):noremap():desc("buffer: prev"),
     ["n|9"] = cmd("bnext"):noremap():desc("buffer: prev"),
-
     -- Misc
     ["n|<leader>nh"] = cmd("noh"):noremap():desc("search: clear highlight"),
 }
