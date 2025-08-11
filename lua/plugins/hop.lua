55592b4440ba88e0721ab7c142640a83337f4db7
Charlie Kenney <me@ch4z.io>
2025-08-11 09:41:50 -0400

    plugins: add hop.nvim

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,33 @@
+return {
+  'smoka7/hop.nvim',
+  version = "*",
+  opts = {
+      keys = 'etovxqpdygfblzhckisuran'
+  },
+  keys = {
+    {
+      ";;",
+      mode = { "n", "x", "o" },
+      "<cmd>HopChar1<cr>",
+      desc = "jump: character",
+    },
+    {
+      ";l",
+      mode = { "n", "x", "o" },
+      "<cmd>HopWord<cr>",
+      desc = "jump: character in-line",
+    },
+    {
+      ",,",
+      mode = { "n", "x", "o" },
+      "<cmd>HopWord<cr>",
+      desc = "jump: word",
+    },
+    {
+      ",l",
+      mode = { "n", "x", "o" },
+      "<cmd>HopWord<cr>",
+      desc = "jump: word in-line",
+    },
+  },
+}--[[@as LazyPluginSpec]]
