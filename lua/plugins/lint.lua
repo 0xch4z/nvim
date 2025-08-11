2fcf4b677f06f9a49b17eb9431dfc05f3cb981d5
Charlie Kenney <charles.kenney@isovalent.com>
2025-08-11 11:59:57 -0400

    plugins: add lint.nvim

---
+++ lua/plugins/lint.lua
@@ -0,0 +1,20 @@
+---@type LazyPluginSpec
+return {
+	"mfussenegger/nvim-lint",
+	event = "BufWritePost",
+	config = function()
+		local linters_by_ft = {
+			go = { "gofmt" },
+			lua = { "luacheck" },
+		}
+		require("lint").linters_by_ft = linters_by_ft
+
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			callback = function()
+				if linters_by_ft[vim.bo.filetype] ~= nil then
+					require("lint").try_lint()
+				end
+			end,
+		})
+	end,
+}
