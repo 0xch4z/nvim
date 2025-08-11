32e6612c408fc70b26b6dde55865be2e9091724b
Charlie Kenney <charles.kenney@isovalent.com>
2025-08-11 13:46:05 -0400

    plugins/treesitter: configure foldmethod/foldexpr

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,14 @@
+---@type LazyPluginSpec
 return {
 	"nvim-treesitter/nvim-treesitter",
 	branch = "master",
 	lazy = false,
 	build = ":TSUpdate",
-} --[[@as LazyPluginSpec]]
+	opts = {},
+	config = function()
+		vim.opt.foldenable = false
+		vim.opt.foldmethod = "expr"
+		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+	end,
+	keys = {},
+}
