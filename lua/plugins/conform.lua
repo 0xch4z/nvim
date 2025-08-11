29ba8d7ecbb5b96e9104fd13555448d28732401c
Charlie Kenney <charles.kenney@isovalent.com>
2025-08-11 11:34:14 -0400

    plugins: add conform.nvim

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  ---@type conform.setupOpts
+  opts = {
+    formatters_by_ft = {
+      go = { "gofmt" },
+      lua = { "stylua" },
+    },
+    format_on_save = {
+      async = true,
+      timeout_ms = 500
+    }
+  }
+}--[[@as LazyPluginSpec]]
