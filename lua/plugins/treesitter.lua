2a5aadd282944905dbb01206166a73d4b232dbc8
Charlie Kenney <charles.kenney@isovalent.com>
2026-03-06 18:41:23 -0500

    plugins/treesitter: use LOCAL_NVIM_TREESITTER_PATH

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,10 @@ return {
 	build = ":TSUpdate",
 	opts = {},
 	config = function()
+		local ts_path = vim.env.LOCAL_NVIM_TREESITTER_PATH
+		if ts_path then
+			vim.opt.rtp:prepend(ts_path)
+		end
 		vim.opt.foldenable = false
 		vim.opt.foldmethod = "expr"
 		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
