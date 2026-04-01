5cc926fdbe0e63eeea1cc68f478ad6aaccfc34fc
Charlie Kenney <charles.kenney@isovalent.com>
2026-04-01 09:20:01 -0400

    plugins: add neogit

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,13 @@
+return {
+	"NeogitOrg/neogit",
+	lazy = true,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"esmuellert/codediff.nvim",
+		"folke/snacks.nvim",
+	},
+	cmd = "Neogit",
+	keys = {
+		{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+	},
+} --[[@as LazyPluginSpec]]
