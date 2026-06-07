832acda411442ee6cf7a22fd8c22d3d6917fb171
Charlie Kenney <me@ch4z.io>
2026-06-07 14:11:33 -0400

    plugins/conform: add cxx clang formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,10 +5,13 @@ return {
 	---@type conform.setupOpts
 	opts = {
 		formatters_by_ft = {
-			go = { "gofmt" },
-			lua = { "stylua" },
-			nix = { "nixfmt" },
-			terraform = { "terraform_fmt" },
+			c = { "clang-format", lsp_format = "fallback" },
+			cpp = { "clang-format", lsp_format = "fallback" },
+			go = { "gofmt", lsp_format = "fallback" },
+			lua = { "stylua", lsp_format = "fallback" },
+			nix = { "nixfmt", lsp_format = "fallback" },
+			rust = { "rustfmt", lsp_format = "fallback" },
+			terraform = { "terraform_fmt", lsp_format = "fallback" },
 		},
 		format_after_save = {
 			async = true,
